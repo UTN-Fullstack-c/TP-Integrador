85187e5826ce208305039a191d5989c5d3cfea58
Martin-Paez <104704565+Martin-Paez@users.noreply.github.com>
2023-11-07 10:45:25 -0300

    Test

---
+++ Backend/FabricaRobot.cs
@@ -0,0 +1,47 @@
+﻿namespace Backend
+{
+    public abstract class Robot
+    {
+        protected Robot(int bateria, int velocidad)
+        {
+            Bateria = bateria;
+            Velocidad = velocidad;
+        }
+
+        public int Bateria { get; set; }
+        public int Velocidad { get; set; }
+
+    }
+
+    public class UAV : Robot
+    {
+        public int Altitud { get; set; }
+
+        public UAV(int bateria, int velocidad, int altitud) 
+            : base(bateria, velocidad)
+        {
+            Altitud = altitud;
+        }
+    }
+
+    public class M8 : Robot
+    {
+        public M8(int bateria, int velocidad) 
+            : base(bateria, velocidad)
+        {
+        }
+    }
+
+    public class FabricaRobot
+    {
+        public UAV CreateUAV()
+        {
+            return new UAV(4000, 150, 3000);
+        }
+        public M8 CreateM8()
+        {
+            return new M8(2500, 80);
+        }
+
+    }
+}
---
+++ FrontEnd/Program.cs
@@ -0,0 +1,18 @@
+﻿using Backend;
+
+namespace FrontEnd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fabrica = new FabricaRobot();
+            UAV uav = fabrica.CreateUAV();
+            M8 m8 = fabrica.CreateM8();
+            Console.WriteLine(uav.Bateria);
+
+            Console.WriteLine();
+            Console.WriteLine(m8.Bateria);
+        }
+    }
+}
